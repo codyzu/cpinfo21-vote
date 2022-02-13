58330a43aea9db46ea5232c137a67be2ac467e20
codyzu <cody.zuschlag@gmail.com>
2022-02-13 11:49:44 +0100

    feat: working auth and vote

--- src/App.js
+++ src/App.js
@@ -1,24 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootswatch/dist/vapor/bootstrap.min.css";
+
+import { Container, Row, Col, Progress, Button } from "reactstrap";
+import app from "./firebase";
+import {
+  getAuth,
+  signInWithRedirect,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  query,
+  doc,
+  setDoc,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (nextUser) => {
+        setUser(nextUser);
+      }),
+    []
+  );
+
+  const [exam, setExam] = useState(0);
+  const [hackathon, setHackathon] = useState(0);
+  useEffect(() => {
+    onSnapshot(query(collection(db, "votes")), (snapshot) => {
+      const data = snapshot.docs.map((d) => d.data());
+      setExam(data.filter((d) => d.exam).length);
+      setHackathon(data.filter((d) => d.hackathon).length);
+    });
+  }, []);
+
+  const votePath = `votes/${user?.uid}`;
+
+  const [vote, setVote] = useState({ exam: false, hackathon: false });
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    return onSnapshot(doc(db, votePath), (snapshot) => {
+      console.log("VOTE", snapshot.data());
+      setVote(snapshot.data());
+    });
+  }, [user, votePath]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="min-vh-100">
+      <Row className="justify-content-end">
+        <Col className="text-end fs-4 align-middle">{user && user.email}</Col>
+        <Col xs="auto">
+          <Button
+            onClick={
+              user
+                ? () => signOut(auth)
+                : () => signInWithRedirect(auth, provider)
+            }
+            color={user ? "secondary" : "success"}
+          >
+            {user ? "Logout" : "Login"}
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>Vote:</Col>
+      </Row>
+      <Row>
+        <Col>Here</Col>
+        <Col>Here</Col>
+      </Row>
+      <Row>
+        <Col>
+          <Progress multi style={{ height: "100px" }}>
+            <Progress
+              animated
+              bar
+              value={exam}
+              max={exam + hackathon}
+              color="danger"
+            >
+              Exam {exam}
+            </Progress>
+            <Progress
+              animated
+              bar
+              value={hackathon}
+              max={exam + hackathon}
+              color="success"
+            >
+              Hackathon {hackathon}
+            </Progress>
+          </Progress>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button
+            disabled={
+              !user || !user.email.endsWith("iut-acy.univ-smb.fr") || vote?.exam
+            }
+            onClick={() =>
+              setDoc(doc(db, votePath), { exam: true, hackathon: false })
+            }
+          >
+            Exam
+          </Button>
+          <Button
+            disabled={
+              !user ||
+              !user.email.endsWith("iut-acy.univ-smb.fr") ||
+              vote?.hackathon
+            }
+            onClick={() =>
+              setDoc(doc(db, votePath), { exam: false, hackathon: true })
+            }
+          >
+            Hackathon
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxY-b_O0soa1SSNws7em7hP8hMXLAoW7c",
+  authDomain: "cpinfo21-final-vote.firebaseapp.com",
+  projectId: "cpinfo21-final-vote",
+  storageBucket: "cpinfo21-final-vote.appspot.com",
+  messagingSenderId: "96559521505",
+  appId: "1:96559521505:web:e228902bec873a3f1521a0",
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
